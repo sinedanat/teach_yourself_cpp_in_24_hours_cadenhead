551fb95c53875816fead43a712c3245e576c5809
ed <>
2021-11-04 14:56:48 +0000

    Add ch01
    FossilOrigin-Name: 1c20f558675feb7b3ccf309fbd5c67ba8877968298ca7c5b2d209fddae220c0b

---
+++ ch01/ex/ex01.cpp
@@ -0,0 +1,11 @@
+// Modify the Motto program to display the text "Saluton Mondo!",
+// the greeting "Hello world!" in the artificial language Esperanto.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Saluton Mondo!\n";
+
+    return 0;
+}
---
+++ ch01/sl/Saluton.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Saluton mondo!\n";
+
+    return 0;
+}
